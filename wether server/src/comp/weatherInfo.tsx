1bf9195d6f3d4622ab245368a23f72789d8b2538
chaim cymerman <a548492309@gmail.com>
2023-09-21 16:00:06 +0300

    first commit

---
+++ wether server/src/comp/weatherInfo.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function weatherInfo(props: any): any {
+  let info = props.info;
+  console.log(info);
+
+  return (
+    <div className="container text-center mt-3">
+      <h2 className="display-4">weather of {info.name}</h2>
+      {info.weather && (
+        <div>
+          <h3>Condition : {info.weather[0].main}</h3>
+          <h3>Temperature : {info.main.temp} c</h3>
+          <h3>Wind : {info.wind.speed} kmh </h3>
+        </div>
+      )}
+    </div>
+  );
+}
